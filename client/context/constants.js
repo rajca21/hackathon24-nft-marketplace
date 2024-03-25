0f3b816b56e63f728c17d24681caf3af36eb6e0d
EmilijaZdravkovic <emilijazdravkovic1998@gmail.com>
2024-03-25 23:12:21 +0100

    Smart contract fix

--- client/context/constants.js
+++ client/context/constants.js
@@ -1,5 +1,5 @@
 import nftMarketplace from './NFTMarketplace.json';
 
 export const NFTMarketplaceAddress =
-  '0x5FbDB2315678afecb367f032d93F642f64180aa3';
+  '0x93305edE30688FF91217DF58f047CEf79Ee7f505';
 export const NFTMarketplaceABI = nftMarketplace.abi;
